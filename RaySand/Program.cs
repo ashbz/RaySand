73a7bef46e3c15c7fc7382f24e1d04310b063758
Ashot Khachatryan <ashot.codes@outlook.com>
2024-02-07 00:07:24 +0000

    Pre cleanup commit

--- RaySand/Program.cs
+++ RaySand/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿global using System.Numerics;
+global using Raylib_CsLo;
+
+global using static Raylib_CsLo.Raylib;
+global using static Raylib_CsLo.RayMath;
+global using static Raylib_CsLo.RayGui;
+global using static Raylib_CsLo.RlGl;
+
+global using static Raylib_CsLo.KeyboardKey;
+global using static Raylib_CsLo.MouseButton;
+global using static Raylib_CsLo.ConfigFlags;
+global using RenderTexture2D = Raylib_CsLo.RenderTexture;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        RaySand.RaySand.main();
+    }
+}
